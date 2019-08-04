7026700f3cfa064c6e9399fb1d89d6f4a5dba1c9
jnaputi <jnaputi253@gmail.com>
2019-08-04 18:43:50 -0500

    Update Item SQL

---
+++ ShoppingCart/Database/Scripts/Procedures/update-item.sql
@@ -0,0 +1,42 @@
+﻿USE ShoppingCart
+
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE sp_UpdateItem
+	@Id INT,
+	@Name NVARCHAR(64),
+	@Description NVARCHAR(128),
+	@Price DECIMAL(16, 4)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	UPDATE Items
+	SET
+		[Name] = @Name,
+		[Description] = @Description,
+		[Price] = @Price
+	WHERE [Id] = @Id
+
+END
+GO
